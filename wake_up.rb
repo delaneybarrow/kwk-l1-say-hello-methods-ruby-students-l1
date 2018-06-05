2c6a071b9dc073df45411b6211011c37a17745c5
delaneybarrow <delaneybarrow19@gmail.com>
2018-06-05 17:22:04 +0000

    automatically backed up by learn

---
+++ wake_up.rb
@@ -0,0 +1,12 @@
+def wake_up
+  puts "hear alarm"
+  5.times do
+    2.times do
+      puts "turn off alarm"
+    end
+  end 
+  puts "check phone"
+  puts "get dressed"
+  puts "brush teeth"
+end
+wake_up
